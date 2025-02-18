108d42b0c6af317eb8c49f06d839eb42919b7e19
Son Nguyen <hoangson091104@gmail.com>
2025-02-18 12:52:25 -0500

    WIP: App Working

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "http://localhost:3000",
   generateRobotsTxt: true, // Generate a robots.txt file
+  changefreq: "daily",
+  priority: 0.8,
+  autoLastmod: true,
   additionalSitemaps: [],
 };
