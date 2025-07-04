ae2ec7305085161141de5897ba36201f7a399aaf
Son Nguyen <hoangson091104@gmail.com>
2025-07-04 10:19:24 -0400

    Final: Add Articles & App Working

---
+++ mocks/mdxMock.js
@@ -0,0 +1,5 @@
+exports.metadata = {
+  title: 'Mock Title',
+  description: 'Mock Description',
+  topics: ['t1','t2']
+};
---
+++ tests/articles.spec.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const { getArticles } = require('../lib/articles');
+
+// Mock out fs and path
+jest.mock('fs/promises');
+jest.mock('path', () => ({ join: (...p) => p.join('/') }));
+
+describe('getArticles()', () => {
+  beforeEach(() => jest.resetAllMocks());
+
+  it('returns only .mdx files with default metadata', async () => {
+    // pretend the content folder has two MDX files and one other
+    fs.readdir.mockResolvedValue(['first.mdx', 'second.mdx', 'ignore.txt']);
+
+    const articles = await getArticles();
+
+    expect(articles).toEqual([
+      { slug: 'first',  title: 'first',  description: '', topics: [] },
+      { slug: 'second', title: 'second', description: '', topics: [] },
+    ]);
+  });
+});
