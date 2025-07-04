61ccbaeb16d501511d9da13d4f38f96ae710075f
Son Nguyen <hoangson091104@gmail.com>
2025-07-04 10:36:04 -0400

    Final: Add Articles & App Working

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }]
-  ]
+module.exports = function (api) {
+  const isTest = api.env('test');
+
+  return {
+    presets: [
+      // This is Next.js’s built-in Babel preset (handles TS, JSX, modern JS, etc).
+      'next/babel',
+
+      // When running Jest, also compile for current Node.
+      isTest && ['@babel/preset-env', { targets: { node: 'current' } }],
+    ].filter(Boolean),
+
+    // No need for plugins here unless you have extras…
+    plugins: [],
+  };
 };
