9c6b0cfc7c2b3fc2091718e83a95a1d94dcd8aa7
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:17:15 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for DevVerse-SWE-Blog.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:17:14Z'
+});
